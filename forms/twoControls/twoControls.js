a2c082149596b1abb7f9f54c0a9005a5d5a47356
Connor Hock <cbh06002@creighton.edu>
2019-09-30 14:09:43 -0500

    initial

---
+++ forms/twoControls/twoControls.js
@@ -0,0 +1,11 @@
+/* Create ONE of the following apps, making new forms as indicated, using appStudio controls and events. Use controls other than buttons or labels, 
+ones that you haven't used before. You aren't limited to Bootstrap4 controls on this exercise, but don't use Bootstrap 3 controls (again, evil).
+Note: You can use a button or label if you need it -it just doesn't count in the grade.
+
+Option A: Create a form named twoControls. In it, create an app that uses two controls you haven't used before.
+
+Requirements:
+    - The controls should be interesting and not simplistic. A label is simplistic, for example.
+    - Change properties of each control from the defaults. Do something visual and note what you changed for each control in a label 
+      on your form (so I don't overlook it). For example, if you make something blue, say so in the label.
+    - Attach one event to each control, and write an event handler - you can just output a message using a simple popup like NSB.MsgBox().
