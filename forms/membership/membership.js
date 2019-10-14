972850fbe0e2f30e60716165458904b65d4f779e
Connor Hock <cbh06002@creighton.edu>
2019-10-13 19:49:20 -0500

    master

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,4 +1,11 @@
-/*
-var members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
-*/
+var members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"];
 
+btnEnter.onclick=function(){
+let firstName= inptFirstName.value;
+console.log(members.includes(firstName))
+  if (members.includes(firstName) == true) {
+  lblMember.value = `Welcome back, ${firstName}`;
+  }
+else {
+  lblMember.value = `You are not a member, ${firstName}`;
+  }
--- forms/twoControls/twoControls.js
+++ forms/twoControls/twoControls.js
@@ -1,13 +1,16 @@
-/* Create ONE of the following apps, making new forms as indicated, using appStudio controls and events. Use controls other than buttons or labels, 
-ones that you haven't used before. You aren't limited to Bootstrap4 controls on this exercise, but don't use Bootstrap 3 controls (again, evil).
-Note: You can use a button or label if you need it -it just doesn't count in the grade.
+jmbControl.onclick=function(){
+  NSB.MsgBox('Congrats! Your mouse works\:\)')
+};
 
-Option A: Create a form named twoControls. In it, create an app that uses two controls you haven't used before.
+sldControl.onchange=function(){
+  NSB.MsgBox('Wow good job!')
+};
 
-Requirements:
-    - The controls should be interesting and not simplistic. A label is simplistic, for example.
-    - Change properties of each control from the defaults. Do something visual and note what you changed for each control in a label 
-      on your form (so I don't overlook it). For example, if you make something blue, say so in the label.
-    - Attach one event to each control, and write an event handler - you can just output a message using a simple popup like NSB.MsgBox().
-    
-*/
+drpdwn.onclick=function(){
+  NSB.MsgBox('I go right instead of left')
+};
+
+
+pgnA.onclick=function(){
+  NSB.MsgBox('Good letter choice')
+};
